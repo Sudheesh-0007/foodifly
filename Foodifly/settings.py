c8acba806492cee1e68bc2f3a9d5222a3501fe29
Sudheesh-0007 <sudheeshchandra8@gmail.com>
2026-04-17 13:39:09 +0530

    database changed to postgre sql

--- Foodifly/settings.py
+++ Foodifly/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,12 @@ WSGI_APPLICATION = 'Foodifly.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'foodifly_db',
+        'USER': 'foodifly_user',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
